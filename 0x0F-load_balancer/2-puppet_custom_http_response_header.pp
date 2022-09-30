ce7b41b7709275c61d91b09dce5299b9aaebd00d
Alatise Oluwaseun <holynation667@gmail.com>
2022-09-30 12:04:22 +0100

    fix: installed HAproxy and setup

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -46,12 +46,13 @@ file { 'Nginx default config file':
 "server {
         listen 80 default_server;
         listen [::]:80 default_server;
-               root /var/www/html;
+        root /var/www/html;
+        add_header X-Served-By $HOSTNAME;
+        
         # Add index.php to the list if you are using PHP
         index index.html index.htm index.nginx-debian.html;
         server_name _;
         location / {
-                add_header X-Served-By $HOSTNAME
                 # First attempt to serve request as file, then
                 # as directory, then fall back to displaying a 404.
                 try_files \$uri \$uri/ =404;
