3f2c2b911bd432ce61eec63c282df2dcbe81be28
Alatise Oluwaseun <holynation667@gmail.com>
2022-09-30 17:15:36 +0100

    fix: error in the config

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -46,7 +46,7 @@ file { 'Nginx default config file':
 "server {
         listen 80 default_server;
         listen [::]:80 default_server;
-        add_header X-Served-By $HOSTNAME;
+        add_header X-Served-By $HOST;
                root /var/www/html;
         # Add index.php to the list if you are using PHP
         index index.html index.htm index.nginx-debian.html;
