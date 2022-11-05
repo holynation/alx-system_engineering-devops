eba799a4630f01d94ab3ef78c8e9c593b4ae2d76
Alatise Oluwaseun <holynation667@gmail.com>
2022-11-05 09:41:55 +0100

    feat: Added APi advanced using REDDIT endpoints as case-study

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Api to query Reddit API for number of subscribers for a given subreddit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+        return number of subscribers for a given subreddit
+        return 0 if invalid subreddit given
+        Invalid subreddits may return a redirect to search results.
+        Ensure that you are not following redirects.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    # setting a custom user agent
+    # https://stackoverflow.com/questions/10606133/ -->
+    # sending-user-agent-using-requests-library-in-python
+    headers = requests.utils.default_headers()
+    headers.update({'User-Agent': 'My User Agent 1.0'})
+
+    r = requests.get(url, headers=headers).json()
+    subscribers = r.get('data', {}).get('subscribers')
+    if not subscribers:
+        return 0
+    return subscribers
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,7 @@
+## 0x16. API advanced
+
+### Resources:
+* [Reddit API](https://www.reddit.com/dev/api/)
+
+### Authors
+Oluwaseun Alatise [![M](https://upload.wikimedia.org/wikipedia/fr/thumb/c/c8/Twitter_Bird.svg/30px-Twitter_Bird.svg.png)](https://twitter.com/Alat_Oluwaseun__)
